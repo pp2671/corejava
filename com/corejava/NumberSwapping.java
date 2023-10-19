8e738bdd3551660de2f322dc4634f4b178097cf1
Saad Patel <142710978+Saadpatel13@users.noreply.github.com>
2023-10-19 16:44:22 +0530

    Add files via upload
    Folder updated inside the parent .com file

---
+++ com/corejava/NumberSwapping.java
@@ -0,0 +1,22 @@
+package corejava;
+
+public class NumberSwapping {
+		
+
+		public static void main(String[] args) {
+			int a = 50;
+			int b = 100;
+			
+			System.out.println("This is before swapping");
+	        System.out.println("a ="+a);
+	        System.out.println("b =" +b);
+	        
+	        int c = a;
+	        a = b;
+	        b = c;
+	        
+	        System.out.println("This is after swapping");
+	        System.out.println("a ="+a);
+	        System.out.println("b =" +b);
+		}
+	}
---
+++ com/corejava/ParameterisedFunction.java
@@ -0,0 +1,16 @@
+package corejava;
+
+public class ParameterisedFunction{
+static int add (int a, int b) {
+	int res = a+b;
+	return res;
+	
+}
+
+	public static void main(String[] args) {
+		
+		System.out.println("Addition is=" + add(5,20));
+		
+	}
+
+}
